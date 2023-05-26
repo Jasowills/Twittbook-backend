d3dedc299f696ff1c9188cb22ff31c84a10312ae
Jasowills <Jujuzhaddy01@gmail.com>
2023-05-26 12:31:14 +0100

    origin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './Items/modules/app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.use(cors());
+
   await app.listen(4000);
 }
 bootstrap();
